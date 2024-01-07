c28ff5e6845ff86958de8be010f15dfd0f60e301
Gururaj <gururajmath3@gmail.com>
2024-01-08 01:02:16 +0530

    router and navbar add

---
+++ client/src/components/navbar.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import "../styles/navbar.css";
+
+function Navbar() {
+  return (
+    <div className="nav-container">
+      <p>Cibo</p>
+      <div className="item-list">
+        <Link to="/">Home</Link>
+        <Link to="/about">About us</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/contact">Contact us</Link>
+      </div>
+      <div className="user">
+        <p>Username</p>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
