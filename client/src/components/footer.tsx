c360508635ba3c3f80001a2872da8688f2d8b40a
Gururaj <gururajmath3@gmail.com>
2024-01-08 10:07:33 +0530

    footer update

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,32 +1,41 @@
+import "boxicons";
 import "../styles/footer.css";
+import "boxicons/css/boxicons.min.css";
+
 function Footer() {
   return (
     <div className="footer-container">
       <div className="footer-content">
         <h2>Services</h2>
         <ul>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
+          <li>Express Delivery</li>
+          <li>24/7 Customer Support</li>
+          <li>Easy Returns</li>
+          <li>Track Your Order</li>
         </ul>
       </div>
       <div className="footer-content">
-        <h2>About</h2>
-        <ul>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
-        </ul>
+        <h2>About Us</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
       </div>
       <div className="footer-content">
-        <h2>Social</h2>
+        <h2>Follow Us</h2>
         <ul>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
+          <li>
+            <i className="bx bxl-facebook social"></i> Facebook
+          </li>
+          <li>
+            <i className="bx bxl-twitter social"></i> Twitter
+          </li>
+          <li>
+            <i className="bx bxl-instagram social"></i> Instagram
+          </li>
+          <li>
+            <i className="bx bxl-linkedin social"></i> LinkedIn
+          </li>
         </ul>
       </div>
     </div>
