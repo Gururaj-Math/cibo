1624b5f200870e014994489b9aeb4cce1b8f574c
Gururaj <gururajmath3@gmail.com>
2024-01-12 23:01:51 +0530

    RoR setup

--- client/src/pages/OrderPage.tsx
+++ client/src/pages/OrderPage.tsx
@@ -40,7 +40,6 @@ function OrderPage() {
         <h1>Featured Items</h1>
         <div className="featured-items">
           <img src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-
           <img src="https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&w=600" />
           <img src="https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=600" />
           <img src="https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg?auto=compress&cs=tinysrgb&w=600" />
