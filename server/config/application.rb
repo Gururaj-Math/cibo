c1b84985f6d62cff7f0ce74f98b799e55116e449
sanket bodke <sanketbodke26@gmail.com>
2024-01-24 16:19:05 +0530

    cors add

--- server/config/application.rb
+++ server/config/application.rb
@@ -2,19 +2,21 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Server
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.middleware.insert_before 0, Rack::Cors do
+    end
     config.load_defaults 7.1
+
+    # Add the following line to autoload the lib directory
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_paths.reject! { |path| path =~ /assets|tasks/ }
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -26,7 +28,11 @@ module Server
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    # Skip views, helpers, and assets when generating a new resource.
     config.api_only = true
+
+    config.session_store :cookie_store, key: 'ciboSession'
+    config.session_store :active_record_store, key: 'ciboSession'
+    config.middleware.use ActionDispatch::Session::CookieStore, key: 'ciboSession'
   end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  post '/users/register', to: 'users#register', defaults: { format: :json }
-  post '/users/login', to: 'users#login', defaults: { format: :json }
+  post 'api/v1/user/register', to: 'users#register', defaults: { format: :json }
+  post 'api/v1/user/login', to: 'users#login', defaults: { format: :json }
 end
