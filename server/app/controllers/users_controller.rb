d5587422de6e9ed1eca9bcda945250c799885246
sanket bodke <sanketbodke26@gmail.com>
2024-01-24 18:21:20 +0530

    user controller typo fix

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,27 +1,23 @@
 class UsersController < ApplicationController
   def register
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
-      render json: { message: 'User registered successfully', data: user }, status: :created
+    if @user.save
+      render json: { message: 'User registered successfully', data: @user }, status: :created
     else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def login
-    user = User.find_by(email: params[:email])
-    email = params[:email]
-    password = params[:password]
-    print(email, password)
-    if user
-      token = generate_token(user)
-      render json: { message: "login Successfully", data: user, token: token}
+    @user = User.find_by(email: params[:email])
+    if @user
+      token = generate_token(@user)
+      render json: { message: "login Successfully", data: @user, token: token}
     else
       render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
   end
-
   private
 
   def user_params
