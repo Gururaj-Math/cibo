771bb97ff4468f396672ea1758c34330811351e7
Gururaj <gururajmath3@gmail.com>
2024-01-24 13:34:17 +0530

    working on food model and controller

--- server/app/models/seller.rb
+++ server/app/models/seller.rb
@@ -1,7 +1,10 @@
 class Seller
   include Mongoid::Document
   include Mongoid::Timestamps
+
   field :id, type: String
   field :name, type: String
   field :location, type: String
+
+  has_many :foods, class_name: 'Food', foreign_key: 'seller_id'
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   post '/users/register', to: 'users#register', defaults: { format: :json }
   post '/users/login', to: 'users#login', defaults: { format: :json }
+
+  # Adding resources for FoodsController
+  resources :foods
 end
