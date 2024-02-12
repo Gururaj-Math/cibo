cd23d4c00dd415be61122dc85903306e22242fa9
Gururaj <gururajmath3@gmail.com>
2024-02-12 14:40:01 +0530

    login/register ui update

--- client/src/pages/auth/forms/login.tsx
+++ client/src/pages/auth/forms/login.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import axios from "axios";
 import API_URI from "../../../constant.ts";
 import { useCookies } from "react-cookie";
@@ -8,6 +8,10 @@ import {
   logInFailure,
 } from "../../../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import { Form, Input, Button } from "antd";
+import "../../../styles/login.css";
+import loginSvg from "../../../assets/login2.svg";
+import { Link } from "react-router-dom";
 
 interface FormData {
   email: string;
@@ -30,14 +34,10 @@ const Login: React.FC = () => {
     });
   };
 
-  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleFormSubmit = async () => {
     try {
       dispatch(logInStart());
-      const response = await axios.post(
-        `${API_URI}/user/login`,
-        formData,
-      );
+      const response = await axios.post(`${API_URI}/user/login`, formData);
       dispatch(logInSuccess(response.data));
       setCookies("access_token", response.data.accessToken);
       console.log(response);
@@ -48,27 +48,47 @@ const Login: React.FC = () => {
   };
 
   return (
-    <>
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleInputChange}
-        />
+    <div className="login-container">
+      <div className="form-container">
+        <Form onFinish={handleFormSubmit}>
+          <h1>Cibo</h1>
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+          </Form.Item>
 
-        <input
-          type="password"
-          name="password_digest"
-          placeholder="Password"
-          value={formData.password_digest}
-          onChange={handleInputChange}
-        />
-
-        <button type="submit">login</button>
-      </form>
-    </>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password
+              type="password"
+              name="password_digest"
+              placeholder="Password"
+              value={formData.password_digest}
+              onChange={handleInputChange}
+            />
+          </Form.Item>
+          <p>New user? <Link to="/register">Register Now!</Link></p>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+      <div className="image-container">
+        <img src={loginSvg} />
+      </div>
+    </div>
   );
 };
 
--- client/src/pages/auth/forms/register.tsx
+++ client/src/pages/auth/forms/register.tsx
@@ -1,6 +1,9 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import axios from "axios";
 import API_URI from "../../../constant.ts";
+import { Link } from "react-router-dom";
+import { Form, Input, Button } from "antd";
+import loginSvg from "../../../assets/login2.svg";
 
 interface FormData {
   name: string;
@@ -25,13 +28,9 @@ const Register: React.FC = () => {
     });
   };
 
-  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleFormSubmit = async () => {
     try {
-      const response = await axios.post(
-        `${API_URI}/user/register`,
-        formData,
-      );
+      const response = await axios.post(`${API_URI}/user/register`, formData);
       console.log(response);
     } catch (error) {
       console.log(error);
@@ -39,40 +38,74 @@ const Register: React.FC = () => {
   };
 
   return (
-    <>
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={formData.name}
-          onChange={handleInputChange}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="phone"
-          placeholder="Phone"
-          value={formData.phone}
-          onChange={handleInputChange}
-        />
-        <input
-          type="password"
-          name="password_digest"
-          placeholder="Password"
-          value={formData.password_digest}
-          onChange={handleInputChange}
-        />
-
-        <button type="submit">Register</button>
-      </form>
-    </>
+    <div className="login-container">
+      <div className="form-container">
+        <Form onFinish={handleFormSubmit}>
+          <h1>Register</h1>
+          <Form.Item
+            name="name"
+            rules={[{ required: true, message: "Please input your name!" }]}
+          >
+            <Input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+          </Form.Item>
+          <Form.Item
+            name="phone"
+            rules={[
+              { required: true, message: "Please input your phone number!" },
+            ]}
+          >
+            <Input
+              type="text"
+              name="phone"
+              placeholder="Phone"
+              value={formData.phone}
+              onChange={handleInputChange}
+            />
+          </Form.Item>
+          <Form.Item
+            name="password_digest"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password
+              type="password"
+              name="password_digest"
+              placeholder="Password"
+              value={formData.password_digest}
+              onChange={handleInputChange}
+            />
+          </Form.Item>
+          <p>
+            Already have an account? <Link to="/login">Login</Link>
+          </p>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+      <div className="image-container">
+        <img src={loginSvg} />
+      </div>
+    </div>
   );
 };
 
