c7bde99a8cebebf3c401c78dbe0fed63db16e695
Gururaj <gururajmath3@gmail.com>
2024-02-24 14:17:20 +0530

    login button add

--- client/src/pages/myAccount.tsx
+++ client/src/pages/myAccount.tsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { Button } from "antd";
 import "../styles/myAccount.css";
 import LeftDrawer from "../components/leftDrawer";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+
 interface RootState {
   user: {
     currentUser: {
       data: {
-        data: {
-          name: string;
-        };
+        name: string;
+        phone: string;
+        email: string;
       };
     } | null;
   };
 }
+
 const MyAccount: React.FC = () => {
   const { currentUser } = useSelector((state: RootState) => state.user);
+  const navigate = useNavigate();
+  const [_, setCookies] = useCookies(["access_token"]);
+  const [loading, setLoading] = useState(false);
+
+  const logout = () => {
+    setLoading(true);
+    setCookies("access_token", "");
+    window.localStorage.clear();
+    setLoading(false);
+    navigate("/login");
+  };
+
   return (
     <div className="accountContainer">
       <div className="accountDetails">
@@ -28,6 +45,11 @@ const MyAccount: React.FC = () => {
         <div>
           <LeftDrawer />
         </div>
+      </div>
+      <div className="logoutButton">
+        <Button type="primary" onClick={logout} loading={loading}>
+          Logout
+        </Button>
       </div>
     </div>
   );
